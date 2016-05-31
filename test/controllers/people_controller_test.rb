078a2eb21106b0e322c016f73d3eabc4f99945de
Mateo Vidal <jvidalba@gmail.com>
2016-05-31 17:19:13 -0500

    Add controller tests

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,7 +1,54 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @person = people(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:people)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create person" do
+    assert_difference('Person.count', 1) do
+      post :create, person: { 
+                              first_name: "Mateo",
+                              last_name: "Vidal",
+                              gender: "Male",
+                              email: "jvidalba@gmail.com",
+                              job: "Software Engineer",
+                              birthdate: "1991-04-02" }
+    end
+    assert_redirected_to person_path(assigns(:person))
+  end
+
+  test "should show person" do
+    get :show, id: @person
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @person
+    assert_response :success
+  end
+
+  test "should update person" do
+    patch :update, id: @person, person: { first_name: "Mateo", last_name: "Vidal" }
+    assert_redirected_to person_path(assigns(:person))
+  end
+
+  test "should destroy person" do
+    assert_difference('Person.count', -1) do
+      delete :destroy, id: @person
+    end
+
+    assert_redirected_to people_path
+  end
 end
